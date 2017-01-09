1cf7cb5615c31b89506f2440b56ff5496cd37633
Max Sandelin <themaxsandelin@gmail.com>
2017-01-09 11:41:40 +0100

    Support for iterations and markup change - The graph now renders labels for both X and Y axis. - The graph now renders iteration labels for both axies. - The graph now renders iteration-lines for both axies.

--- graph.js
+++ graph.js
@@ -1,70 +1,192 @@
-function Graph (container, parameters) {
-  this.container = container;
-  this.parameters = parameters;
-  
-  // Defines the container selector and adds any missing classes to it
-  function defineContainer (container) {
-    this.container = document.querySelector(container);
+class Graph {
+
+  constructor (container, parameters) {
+    this.container = container;
+    this.parameters = parameters;
+    this.stats = {};
+    this.defaults = {
+      iterations: 2
+    };
+
+    document.addEventListener('DOMContentLoaded', () => {
+      this.defineContainer();
+      this.buildGraphElements();
+      this.calculateStats();
+      this.renderAxisLabels();
+      this.renderIterations();
+    });
+  }
+
+  defineContainer () {
+    this.container = document.querySelector(this.container);
     if (!this.container.classList.contains('graphContainer')) this.container.classList.add('graphContainer');
   }
-  
-  // Creates and appends all neccessary elements for the graph
-  function buildGraphElements () {
-    
+
+  buildGraphElements () {
     // Builds both axis elements with required child elements.
     function buildAxisElements (container) {
       // X-Axis build
-      var xAxis = document.createElement('div');
+      const xAxis = document.createElement('div');
       xAxis.classList.add('axis');
       xAxis.classList.add('x');
-      
-      var xLabel = document.createElement('div');
+
+      const xLabel = document.createElement('div');
       xLabel.classList.add('label');
       xAxis.appendChild(xLabel);
-      
-      var xIterations = document.createElement('div');
+
+      const xIterations = document.createElement('div');
       xIterations.classList.add('iterations');
+
+      const xIterationsInner = document.createElement('div');
+      xIterationsInner.classList.add('inner');
+      xIterations.appendChild(xIterationsInner);
+
+      const xLines = document.createElement('div');
+      xLines.classList.add('lines');
+
+      xAxis.appendChild(xLines);
       xAxis.appendChild(xIterations);
-      
+
       // Y-Axis build
-      var yAxis = document.createElement('div');
+      const yAxis = document.createElement('div');
       yAxis.classList.add('axis');
       yAxis.classList.add('y');
-      
-      var yLabel = document.createElement('div');
+
+      const yLabel = document.createElement('div');
       yLabel.classList.add('label');
       yAxis.appendChild(yLabel);
-      
-      var yIterations = document.createElement('div');
+
+      const yIterations = document.createElement('div');
       yIterations.classList.add('iterations');
+
+      const yIterationsInner = document.createElement('div');
+      yIterationsInner.classList.add('inner');
+      yIterations.appendChild(yIterationsInner);
+
+      const yLines = document.createElement('div');
+      yLines.classList.add('lines');
+
+      yAxis.appendChild(yLines);
       yAxis.appendChild(yIterations);
-      
+
       container.appendChild(yAxis);
       container.appendChild(xAxis);
     }
-    
+
     function buildGraph (container) {
-      var wrapper = document.createElement('div');
+      const wrapper = document.createElement('div');
       wrapper.classList.add('graphWrapper');
-      
-      var graph = document.createElement('div');
+
+      const graph = document.createElement('div');
       graph.classList.add('graph');
-      
+
       wrapper.appendChild(graph);
       container.appendChild(wrapper);
     }
-    
+
+    this.container.innerHTML = '';
+
     buildAxisElements(this.container);
     buildGraph(this.container);
   }
-  
-  // Initial setup scripts that runs once the DOM is loaded
-  function setup () {
-    defineContainer(this.container);
-    buildGraphElements();
+
+  calculateStats () {
+    const entries = this.parameters.entries;
+    const axies = {
+      x: {
+        id: 'x',
+        iterations: {
+          count: this.defaults.iterations,
+          size: 0,
+          value: 0
+        },
+        element: this.container.querySelector('.axis.x'),
+        max: 0,
+        total: 0,
+        label: this.parameters.labels.x
+      },
+      y: {
+        id: 'y',
+        iterations: {
+          count: this.defaults.iterations,
+          size: 0,
+          value: 0
+        },
+        element: this.container.querySelector('.axis.y'),
+        max: 0,
+        total: 0,
+        label: this.parameters.labels.y
+      }
+    };
+
+    for (let a = 0; a < entries.length; a++) {
+      const entry = entries[a];
+      axies.x.max = (axies.x.max < entry.x) ? entry.x:axies.x.max;
+      axies.y.max = (axies.y.max < entry.y) ? entry.x:axies.y.max;
+
+      axies.x.total += entry.x;
+      axies.y.total += entry.y;
+    }
+
+    const graph = {
+      element: this.container.querySelector('.graph'),
+      width: this.container.querySelector('.graph').offsetWidth,
+      height: this.container.querySelector('.graph').offsetHeight
+    };
+
+    if (this.parameters.iterations !== null) {
+      if (this.parameters.iterations !== null) axies.x.iterations.count = this.parameters.iterations.x;
+      if (this.parameters.iterations !== null) axies.y.iterations.count = this.parameters.iterations.y;
+    }
+
+    axies.x.iterations.size = graph.width / (axies.x.iterations.count + 1);
+    axies.x.iterations.value = parseFloat(axies.x.max / (axies.x.iterations.count + 1)).toFixed(1);
+
+    axies.y.iterations.size = graph.height / (axies.y.iterations.count + 1);
+    axies.y.iterations.value = parseFloat(axies.y.max / (axies.y.iterations.count + 1)).toFixed(1);
+
+    this.stats = {
+      axies: axies,
+      graph: graph
+    };
   }
-  
-  document.addEventListener('DOMContentLoaded', function () {
-    setup.bind(this)();
-  }.bind(this));
+
+  renderAxisLabels () {
+    for (let i = 0; i < 2; i++) {
+      const axis = (i === 0) ? this.stats.axies.x:this.stats.axies.y;
+      axis.element.querySelector('.label').innerText = axis.label;
+    }
+  }
+
+  renderIterations (axis) {
+
+    function render (axis) {
+      const count = axis.iterations.count + 2; // Add 2 to accomodate for origo(0) and max value
+
+      for (let x = 0; x < count; x++) {
+        const element = axis.element.querySelector('.iterations .inner');
+        const value = x * axis.iterations.value;
+
+        const label = document.createElement('p');
+        label.innerText = (x > 0 && x < (count - 1)) ? value:((x === 0) ? 0:axis.max);
+
+        if (x > 0 && x < (count - 1)) {
+          label.style[(axis.id === 'x') ? 'left':'top'] = (axis.iterations.size * x) + 'px';
+
+          const lines = axis.element.querySelector('.lines');
+          const line = document.createElement('div');
+          line.classList.add('line');
+          line.style[(axis.id === 'x') ? 'left':'top'] = (axis.iterations.size * x) + 'px';
+
+          lines.appendChild(line);
+        }
+
+        element.appendChild(label);
+      }
+    }
+
+    if (axis !== 'y') render(this.stats.axies.x);
+    if (axis !== 'x') render(this.stats.axies.y);
+  }
+
 }
